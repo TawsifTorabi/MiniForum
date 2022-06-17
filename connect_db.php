8ecb28f8892bd18fd85c00c19b83e9774069fe72
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2022-06-17 14:12:06 +0600

    Delete connect_db.php

--- connect_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-mysql_connect('localhost', 'root', '') or die(mysql_error());
-mysql_select_db("forum") or die(mysql_error());
-$con = mysqli_connect("localhost", "root", "", "forum");
-
-?>
-
